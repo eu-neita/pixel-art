93dc0e01ea8e1c527b06a5c0a2489029ef3c42a3
neita <neitass.music@gmail.com>
2022-12-14 16:01:46 -0300

    centraliza tudo e adiciona a paleta cor 1 preta

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const createDivsPallet = () => {
-    const getPallete = document.getElementById('color-palette');
-for (let index = 0; index < 4; index += 1) {
+const createDivsPallete = () => {
+  const getPallete = document.getElementById('color-palette');
+  for (let index = 0; index < 4; index += 1) {
     const createDiv = document.createElement('div');
     createDiv.className = 'color';
     createDiv.style.border = 'solid 1px black';
     createDiv.style.width = '50px';
     createDiv.style.height = '50px';
     getPallete.appendChild(createDiv);
-}
+  }
 };
 
+const colorPallete = () =>{
+  const getColorPallete = document.querySelectorAll('.color');
+  getColorPallete[0].style.backgroundColor = 'black';
+}
+
 window.onload = () => {
-  createDivsPallet();
+  createDivsPallete();
+  colorPallete();
 };
